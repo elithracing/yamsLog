d483c052c39df29fa07b25bb7279bded7be3dcac
Per Öberg <per@familjenoberg.se>
2014-10-14 22:29:06 +0200

    Added GPL-license in all java files

--- clients/android/src/se/liu/tddd77/bilsensor/list/DynamicEventButton.java
+++
@@ -1,5 +0,0 @@
-//package se.liu.tddd77.bilsensor.list;
-//
-//public class DynamicEventButton {
-//
-//}
--- clients/android/src/se/liu/tddd77/bilsensor/listeners/SensorStatusListener.java
+++
@@ -1,9 +0,0 @@
-//package FrontendConnection;
-//
-//import se.liu.tddd77.bilsensor.data.Sensor;
-//
-//public interface SensorStatusListener {
-//
-//	public void statusChanged(Sensor sensor);
-//	
-//}
